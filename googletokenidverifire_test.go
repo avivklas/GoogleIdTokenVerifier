5b6a87f98898c51af1fb22501d2a1fc997760158
aviv <avivklas@gmail.com>
2017-06-20 23:15:53 +0300

    improved api

--- googletokenidverifire_test.go
+++ googletokenidverifire_test.go
@@ -5,7 +5,10 @@ import "testing"
 func TestCheckToken(t *testing.T) {
 	authToken := "XXXXXXXXXXX.XXXXXXXXXXXX.XXXXXXXXXX"
 	aud := "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.apps.googleusercontent.com"
-	actual := Verify(authToken, aud)
+	actual, err := Verify(authToken, aud)
+	if err != nil {
+		t.Error(err)
+	}
 	var token *TokenInfo
 	expected := token
 	if actual != expected {
